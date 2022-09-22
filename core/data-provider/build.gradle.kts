405234b3e9e95989cb32f09073bda3866c8a97b9
Aristofanis <aristofanis.zisis@gmail.com>
2022-09-22 11:30:21 +0300

    Build: Add hilt di to data provider module

--- core/data-provider/build.gradle.kts
+++ core/data-provider/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("android-common-build")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
 }
 
 dependencies {
     implementation(Libs.dataStoreAndroidLibrary)
+    implementation(Libs.hiltAndroidLibrary)
+    implementation(Libs.hiltKaptCompilerLibrary)
 }
