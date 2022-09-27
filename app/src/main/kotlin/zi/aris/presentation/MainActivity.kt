4763b5c7bc6d6ab36cae30e6ddd13fba21e9d625
Aristofanis <aristofanis.zisis@gmail.com>
2022-09-28 01:01:41 +0300

    Refactor: Move logic to fragment

--- app/src/main/kotlin/zi/aris/presentation/MainActivity.kt
+++ app/src/main/kotlin/zi/aris/presentation/MainActivity.kt
@@ -1,37 +1,8 @@
 package zi.aris.presentation
 
-import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import zi.aris.useronboarding.R
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.main_activity) {
-
-    private val viewModel: MainViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        registerObservers()
-    }
-
-
-    private fun registerObservers() {
-        lifecycleScope.launch { viewModel.state.collect { navigateUser(it.navChooser) } }
-    }
-
-    private fun navigateUser(navState: MainScreenContract.UserNavOptions) {
-
-        val navId = when (navState) {
-            is MainScreenContract.UserNavOptions.NavigateToPinSignIn -> zi.aris.pin.R.navigation.pin_nav_graph
-            else -> zi.aris.onboarding.R.navigation.onboarding_nav_graph
-        }
-
-        val hostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        hostFragment?.findNavController()?.setGraph(navId)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
--- app/src/main/kotlin/zi/aris/presentation/MainFragment.kt
+++ app/src/main/kotlin/zi/aris/presentation/MainFragment.kt
@@ -1,7 +1,37 @@
 package zi.aris.presentation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import zi.aris.useronboarding.R
 
-class MainFragment: Fragment(R.layout.main_fragment) {
+@AndroidEntryPoint
+class MainFragment : Fragment(R.layout.main_fragment) {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        registerObservers()
+    }
+
+    private fun registerObservers() {
+        lifecycleScope.launch { viewModel.state.collect { navigateUser(it.navChooser) } }
+    }
+
+    private fun navigateUser(navState: MainScreenContract.UserNavOptions) {
+
+        val navId = when (navState) {
+            is MainScreenContract.UserNavOptions.NavigateToPinSignIn -> zi.aris.pin.R.navigation.pin_nav_graph
+            else -> zi.aris.onboarding.R.navigation.onboarding_nav_graph
+        }
+
+        val hostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        hostFragment?.findNavController()?.setGraph(navId)
+    }
 }
